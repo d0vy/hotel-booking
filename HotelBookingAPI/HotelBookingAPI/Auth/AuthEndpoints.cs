7a0875c7db92889e76ba45782d560904e950ca95
Dovydas Čiapas <9512042+d0vy@users.noreply.github.com>
2024-11-03 23:50:20 +0200

    add refresh token

--- HotelBookingAPI/HotelBookingAPI/Auth/AuthEndpoints.cs
+++ HotelBookingAPI/HotelBookingAPI/Auth/AuthEndpoints.cs
@@ -3,6 +3,8 @@ using HotelBookingAPI.Auth.Model;
 using HotelBookingAPI.Auth.Services;
 using Microsoft.AspNetCore.Identity;
 using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace HotelBookingAPI.Auth
 {
@@ -35,14 +37,14 @@ namespace HotelBookingAPI.Auth
                 return Results.Created();
             });
 
-            app.MapPost("api/login", async (UserManager<HotelUser> userManager, JwtTokenService jwtTokenService, LoginDTO dto) =>
+            app.MapPost("api/login", async (UserManager<HotelUser> userManager, JwtTokenService jwtTokenService, HttpContext httpContext,LoginDTO dto) =>
             {
                 var user = await userManager.FindByNameAsync(dto.UserName);
                 if (user == null)
                 {
                     return Results.UnprocessableEntity("Username does not exist");
                 }
-                Console.WriteLine(user.UserName);
+
                 var isPasswordValid = await userManager.CheckPasswordAsync(user, dto.Password);
                 if(!isPasswordValid)
                 {
@@ -51,7 +53,55 @@ namespace HotelBookingAPI.Auth
 
                 var roles = await userManager.GetRolesAsync(user);
 
+                var expiresAt = DateTime.UtcNow.AddDays(3);
                 var accessToken = jwtTokenService.CreateAccessToken(user.UserName, user.Id, roles);
+                var refreshToken = jwtTokenService.CreatRefreshToken(user.Id, expiresAt);
+
+                var cookieOptions = new CookieOptions
+                {
+                    HttpOnly = true,
+                    SameSite = SameSiteMode.Lax,
+                    Expires = expiresAt,
+                };
+
+                httpContext.Response.Cookies.Append("RefreshToken", refreshToken, cookieOptions);
+
+                return Results.Ok(new SuccessfulLoginDTO(accessToken));
+            });
+
+            app.MapPost("api/accessToken", async (UserManager<HotelUser> userManager, JwtTokenService jwtTokenService, HttpContext httpContext) =>
+            {
+                if(!httpContext.Request.Cookies.TryGetValue("RefreshToken", out var refreshToken))
+                {
+                    return Results.UnprocessableEntity();
+                }
+
+                if(!jwtTokenService.TryParseRefreshToken(refreshToken, out var claims))
+                {
+                    return Results.UnprocessableEntity();
+                }
+
+                var userId = claims.FindFirstValue(JwtRegisteredClaimNames.Sub);
+                var user = await userManager.FindByIdAsync(userId);
+                if(user == null)
+                {
+                    return Results.UnprocessableEntity();
+                }
+
+                var roles = await userManager.GetRolesAsync(user);
+
+                var expiresAt = DateTime.UtcNow.AddDays(3);
+                var accessToken = jwtTokenService.CreateAccessToken(user.UserName, user.Id, roles);
+                var newRefreshToken = jwtTokenService.CreatRefreshToken(user.Id, expiresAt);
+
+                var cookieOptions = new CookieOptions
+                {
+                    HttpOnly = true,
+                    SameSite = SameSiteMode.Lax,
+                    Expires = expiresAt,
+                };
+
+                httpContext.Response.Cookies.Append("RefreshToken", refreshToken, cookieOptions);
 
                 return Results.Ok(new SuccessfulLoginDTO(accessToken));
             });
